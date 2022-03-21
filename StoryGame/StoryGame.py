750c0af589dfaadb98615d099bd14a37029c2e5a
ybiblow <ybiblow@gmail.com>
2022-03-21 02:48:49 +0200

    first commit

---
+++ StoryGame/StoryGame.py
@@ -0,0 +1,22 @@
+from question_generator.questiongenerator import QuestionGenerator
+from os import listdir
+
+
+class StoryGame:
+    def __init__(self):
+        #self.qg = QuestionGenerator()
+        pass
+
+    def showListOfStories(self):
+        self.list_of_stories = listdir('StoryGame/dataset')
+        print("List of stories: ")
+        for story in self.list_of_stories:
+            print(story)
+
+    def chooseStory(self):
+        user_input = input("Enter number: ")
+        try:
+            num = int(user_input)
+            print(self.list_of_stories)
+        except ValueError:
+            print("That's not an int!")
---
+++ StoryGame/main.py
@@ -0,0 +1,18 @@
+import sys
+from os import listdir
+import time
+
+from StoryGame import StoryGame
+
+print(sys.executable)
+print('hello world!')
+print(listdir("storygame/dataset"))
+#sorted(listdir('StoryGame/dataset'))
+# sg = StoryGame()
+# sg.showListOfStories()
+# sg.chooseStory()
+
+# start = time.time()
+# sg.generateQuestions()
+# end = time.time()
+# print('Time for creating QG: ', end - start)
